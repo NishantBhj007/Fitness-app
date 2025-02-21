f9a7a2f2a37356088d3b1c5e87dffb7e03ff5e7a
Nishant Bhardwaj <nishantbj2020@gmail.com>
2025-02-21 23:49:49 +0530

    Update ExerciseCard.jsx

--- src/components/ExerciseCard.jsx
+++ src/components/ExerciseCard.jsx
@@ -1,52 +1,84 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { Button, Stack, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import Pagination from "@mui/material/Pagination";
+import { Box, Stack, Typography } from "@mui/material";
 
-const ExerciseCard = ({ exercise }) => {
-  console.log(exercise);
+import { exerciseOptions, fetchData } from "../utils/fetchData";
+import ExerciseCard from "./ExerciseCard";
+
+function Exercises({ exercises, setExercises, bodyPart }) {
+  console.log(exercises);
+  const [currentPage, setCurrentPage] = useState(1);
+  const exercisesPerPage = 9;
+  const indexOfLastExercise = currentPage * exercisesPerPage;
+  const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage;
+
+  const currentExercises = exercises.slice(
+    indexOfFirstExercise,
+    indexOfLastExercise
+  );
+
+  const paginate = (e, value) => {
+    setCurrentPage(value);
+    window.scrollTo({ top: 1800, behavior: "smooth" });
+  };
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      let exercisesData = [];
+
+      if (bodyPart === "all") {
+        exercisesData = await fetchData(
+          "https://exercisedb.p.rapidapi.com/exercises",
+          exerciseOptions
+        );
+      } else {
+        exercisesData = await fetchData(
+          `https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`,
+          exerciseOptions
+        );
+      }
+
+      setExercises(exercisesData);
+    };
+
+    fetchExercisesData();
+  }, [bodyPart]);
 
   return (
-    <Link className="exercise-card" to={`/exercise/${exercise.id}`}>
-      <img src={exercise.gifUrl} alt={exercise.name} loading="lazy" />
-      <Stack direction="row">
-        <Button
-          sx={{
-            ml: "21px",
-            color: "#fff",
-            background: "#FFA9A9",
-            fontSize: "14px",
-            borderRadius: "20px",
-            textTransform: "capitalize",
-          }}
-        >
-          {exercise.bodyPart}
-        </Button>
-        <Button
-          sx={{
-            ml: "21px",
-            color: "#fff",
-            background: "#FCC757",
-            fontSize: "14px",
-            borderRadius: "20px",
-            textTransform: "capitalize",
-          }}
-        >
-          {exercise.target}
-        </Button>
-      </Stack>
+    <Box id="exercises" sx={{ mt: { lg: "109px" } }} mt="50px" p="20px">
       <Typography
-        ml="21px"
-        color="#000"
+        variant="h4"
         fontWeight="bold"
-        sx={{ fontSize: { lg: "24px", xs: "20px" } }}
-        mt="11px"
-        pb="10px"
-        textTransform="capitalize"
+        sx={{ fontSize: { lg: "44px", xs: "30px" } }}
+        mb="46px"
       >
-        {exercise.name}
+        Showing Results
       </Typography>
-    </Link>
+      <Stack
+        direction="row"
+        sx={{ gap: { lg: "107px", xs: "50px" } }}
+        flexWrap="wrap"
+        justifyContent="center"
+      >
+        {currentExercises.map((exercise, idx) => {
+          return <ExerciseCard key={idx} exercise={exercise} />;
+        })}
+      </Stack>
+      <Stack sx={{ mt: { lg: "114px", xs: "70px" } }} alignItems="center">
+        {exercises.length > 9 && (
+          <Pagination
+            color="standard"
+            shape="rounded"
+            defaultPage={1}
+            count={Math.ceil(exercises.length / exercisesPerPage)}
+            page={currentPage}
+            onChange={paginate}
+            size="large"
+          />
+        )}
+      </Stack>
+    </Box>
   );
-};
+}
 
-export default ExerciseCard;
+export default Exercises;
